82d282c1329061f2e63991959e42d3db6e633718
Louis Lam <louislam@live.com>
2015-08-15 12:13:16 +0800

    add js

---
+++ view/RawCRUDTheme/js/LouisCRUD.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Louis Lam on 8/15/2015.
+ */
+var LouisCRUD = (function () {
+    function LouisCRUD() {
+        var self = this;
+        $(document).ready(function () {
+            self.table = $('#table').DataTable();
+            // Delete Button
+            $(".btn-delete").click(function () {
+                var result = window.confirm("Are you sure?");
+                if (result) {
+                    var btn = $(this);
+                    var deleteLink = $(this).data("url");
+                    $.ajax({
+                        url: deleteLink,
+                        type: "DELETE"
+                    }).done(function (data) {
+                        $("#row-" + btn.data("id")).remove();
+                    });
+                }
+            });
+            // Ajax Form
+            $("form.ajax").submit(function () {
+                $.ajax({
+                    url: $(this).attr("action"),
+                    type: $(this).data("method"),
+                    data: $(this).serialize()
+                }).done(function (data) {
+                    alert(data);
+                });
+                return false;
+            });
+        });
+    }
+    return LouisCRUD;
+})();
+//# sourceMappingURL=LouisCRUD.js.map
--- view/RawCRUDTheme/scripts.php
+++ view/RawCRUDTheme/scripts.php
@@ -10,7 +10,7 @@ use LouisLam\CRUD\Util;
 
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-<script src="<?=Util::url("js/LouisCRUD.js") ?>"></script>
+<script src="<?=Util::url("vendor/louislam/louislam-crud-library/js/LouisCRUD.js") ?>"></script>
 <script>
     var crud = new LouisCRUD();
 </script>
