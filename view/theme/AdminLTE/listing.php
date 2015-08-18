a5bb6ce75008b6d266ac836f6656ae89a0bf54bc
Louis Lam <louislam@live.com>
2015-08-18 16:03:15 +0800

    update dataTables

--- view/theme/AdminLTE/listing.php
+++ view/theme/AdminLTE/listing.php
@@ -22,7 +22,7 @@ $this->layout($layoutName, [
         <div class="box">
 
             <div class="box-header">
-                <h3 class="box-title">Data Table With Full Features</h3>
+                <h3 class="box-title"></h3>
             </div>
 
             <div class="box-body">
