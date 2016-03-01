fd51c698317c466c64ad82e3a235bdd7023a1869
Louis Lam <louislam@live.com>
2016-03-01 12:57:10 +0800

    remove save btn in CKeditor

--- src/LouisLam/CRUD/FieldType/CKEditor.php
+++ src/LouisLam/CRUD/FieldType/CKEditor.php
@@ -71,7 +71,7 @@ HTML;
                         ckConfig.toolbar = 'Full';
 
                         ckConfig.toolbar_Full = [
-                            { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+                            { name: 'document', items : [ 'Source','-', 'NewPage','DocProps','Preview','Print','-','Templates' ] },
                             { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
                             { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
                             { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
