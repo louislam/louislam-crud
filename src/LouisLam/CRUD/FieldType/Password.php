a8d93b973d47c7449940faea06d2e7e1e7c2f1e0
Louis Lam <louislam@live.com>
2016-05-02 15:01:43 +0800

    use password_hash instead of md5

--- src/LouisLam/CRUD/FieldType/Password.php
+++ src/LouisLam/CRUD/FieldType/Password.php
@@ -12,12 +12,30 @@ namespace LouisLam\CRUD\FieldType;
 class Password extends TextField
 {
 
+    /**
+     * @var callback
+     */
+    private $encryptionClosure = null;
+
     public function __construct()
     {
         $this->type = "password";
+
+        // Use MD5 by default
+        $this->encryptionClosure = function ($v) {
+            return password_hash($v, PASSWORD_DEFAULT);
+        };
     }
 
-    public function renderCell($value) {
+    public function renderCell($value)
+    {
         return "***";
+    }
+
+    public function beforeStoreValue($valueFromUser)
+    {
+        $c = $this->encryptionClosure;
+
+        return $c($valueFromUser);
     }
 }
