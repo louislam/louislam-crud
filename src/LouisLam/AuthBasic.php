a35fbddd928d3f30d53901501bceb6354a19d147
Louis Lam <louislam@users.noreply.github.com>
2017-10-13 16:20:26 +0800

    make Auth adaptable

---
+++ src/LouisLam/AuthBasic.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User5
+ * Date: 10/13/2017
+ * Time: 3:34 PM
+ */
+
+namespace LouisLam;
+
+
+abstract class AuthBasic
+{
+    /**
+     * @var callable
+     */
+    protected $encryptPasswordFunction = null;
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @return mixed
+     */
+    public abstract function login($username, $password);
+
+    /**
+     * @param bool $force
+     * @return mixed
+     */
+    public abstract function getUser($force = false);
+
+    /**
+     * @return boolean
+     */
+    public abstract function isLoggedIn();
+
+    /**
+     * @param callable $callback
+     * @return void
+     */
+    public abstract function checkLogin($callback);
+
+    /**
+     * @return void
+     */
+    public abstract function logout();
+
+    public function setEncryptPasswordFunction($func) {
+        $this->encryptPasswordFunction = $func;
+    }
+
+}
---
+++ src/LouisLam/LouisAuth.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User5
+ * Date: 10/13/2017
+ * Time: 3:34 PM
+ */
+
+namespace LouisLam;
+
+
+use RedBeanPHP\OODBBean;
+use RedBeanPHP\R;
+
+class LouisAuth extends AuthBasic
+{
+
+    /**
+     * @var OODBBean
+     */
+    private $user = null;
+
+
+    public function login($username, $password)
+    {
+        $row = R::getRow("SELECT * FROM `user` WHERE username = ? ", array(
+            $username
+        ));
+
+        if ($row != null && password_verify($password, $row["password"]) ) {
+
+            $_SESSION["username"] = $username;
+            $_SESSION["password"] = $password;
+            return true;
+        } else {
+            unset( $_SESSION["username"]);
+            unset($_SESSION["password"] );
+            return false;
+        }
+    }
+
+    public function getUser($force = false)
+    {
+
+        if (!isset($_SESSION["username"]) || !isset($_SESSION["password"])) {
+            return null;
+        }
+
+        if ($force || $this->user == null) {
+
+            $bean = R::findOne("user", " username = ? ", array(
+                $_SESSION["username"]
+            ));
+
+            if ($bean != null && password_verify($_SESSION["password"], $bean->password) ) {
+                $this->user = $bean;
+                return $this->user;
+            } else {
+                return null;
+            }
+
+        } else {
+            return $this->user;
+        }
+    }
+
+    public function isLoggedIn()
+    {
+        if (!isset($_SESSION["username"]) || !isset($_SESSION["password"])) {
+            return false;
+        }
+
+        $row = $this->getUser(true);
+
+        if ($row != null) {
+            return true;
+        } else {
+            unset( $_SESSION["username"]);
+            unset($_SESSION["password"] );
+            return false;
+        }
+    }
+
+    public function checkLogin($callback)
+    {
+        if (! $this->isLoggedIn()) {
+            $callback();
+        }
+    }
+
+    public function logout()
+    {
+        unset($_SESSION["username"]);
+        unset($_SESSION["password"]);
+    }
+
+}
