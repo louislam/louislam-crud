011a33c3302e9e64a58dd0bea046b03fdc3ad49f
Louis Lam <louislam@kotech.hk>
2018-09-18 17:03:35 +0800

    update GoogleMap to support api key and click to pin

--- src/LouisLam/CRUD/FieldType/GoogleMap.php
+++ src/LouisLam/CRUD/FieldType/GoogleMap.php
@@ -13,9 +13,8 @@ use LouisLam\CRUD\Field;
 
 class GoogleMap extends FieldType
 {
-
+    protected static $apiKey = null;
     protected $type = "text";
-
     protected $longitudeField;
 
     /**
@@ -40,61 +39,60 @@ class GoogleMap extends FieldType
         $readOnly = $this->getReadOnlyString();
         $required = $this->getRequiredString();
         $crud = $this->field->getCRUD();
-
+        $key = self::$apiKey;
         $longitudeFieldName = $this->longitudeField->getName();
 
         $html  = <<< HTML
         <div class="form-group">
-            <label for="field-$name">$display</label> <input id="field-$name" class="form-control"  type="hidden" name="$name" value="$value" $readOnly $required />
- <div id="map-$name" style="width:500px; height:500px"></div>
-
-
-
+            <label for="field-$name">$display</label>
+            <input id="field-$name" class="form-control"  type="hidden" name="$name" value="$value" $readOnly $required />
+            <div id="map-$name" style="width:100%; height:500px"></div>
         </div>
 HTML;
 
         $crud->addScript(<<< HTML
         
 
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=$key"></script>
         
-  <script>
-        $(document).ready(function () {
+<script>
+    $(document).ready(function () {
+        var latitude =  $("#field-$name").val();
+        var longitude = $("#field-$longitudeFieldName").val();
+        var zoom = 12;
+        var LatLng = new google.maps.LatLng(latitude, longitude);
 
+        var mapOptions = {
+              zoom: zoom,
+              center: LatLng,
+              panControl: false,
+              zoomControl: false,
+              scaleControl: true,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
 
-    var latitude =  $("#field-$name").val();
-    var longitude = $("#field-$longitudeFieldName").val();
-    var zoom = 7;
+        var map = new google.maps.Map(document.getElementById("map-$name"), mapOptions);
 
-    var LatLng = new google.maps.LatLng(latitude, longitude);
-
-    var mapOptions = {
-      zoom: zoom,
-      center: LatLng,
-      panControl: false,
-      zoomControl: false,
-      scaleControl: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    var map = new google.maps.Map(document.getElementById("map-$name"),mapOptions);
-
-    var marker = new google.maps.Marker({
-      position: LatLng,
-      map: map,
-      title: 'Drag Me!',
-      draggable: true
-    });
-
-    google.maps.event.addListener(marker, 'dragend', function(marker){
-      var latLng = marker.latLng;
-
-      $("#field-$name").val(latLng.lat());
-      $("#field-$longitudeFieldName").val(latLng.lng());
-    });
+        var marker = new google.maps.Marker({
+            position: LatLng,
+            map: map,
+            title: 'Drag Me!',
+            draggable: true
+        });
+        
+        google.maps.event.addListener(map, 'click', function(event) {
+           marker.setPosition(event.latLng);
+           $("#field-$name").val(latLng.lat());
+           $("#field-$longitudeFieldName").val(latLng.lng());
         });
 
-  </script>
+        google.maps.event.addListener(marker, 'dragend', function(marker) {
+            var latLng = marker.latLng;
+            $("#field-$name").val(latLng.lat());
+            $("#field-$longitudeFieldName").val(latLng.lng());
+        });
+    });
+</script>
 HTML
         );
 
@@ -104,5 +102,8 @@ HTML
         return $html;
     }
 
+    public static function setAPIKey($apiKey) {
+        self::$apiKey = $apiKey;
+    }
 
 }
